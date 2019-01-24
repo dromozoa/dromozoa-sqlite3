9d382c609a711282a00ff6bd375c3d7fc617b045
Tomoyuki Fujimori <moyu@dromozoa.com>
2019-01-25 08:15:08 +0900

    テストを修正。

--- test/test_sharable.lua
+++ test/test_sharable.lua
@@ -15,7 +15,69 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-sqlite3.  If not, see <http://www.gnu.org/licenses/>.
 
+local multi = require "dromozoa.multi"
+local unix = require "dromozoa.unix"
 local sqlite3 = require "dromozoa.sqlite3"
 
-local dbh = assert(sqlite3.open_sharable ":memory:")
-assert(dbh:close())
+local verbose = os.getenv "VERBOSE" == "1"
+
+local flags = unix.bor(
+    sqlite3.SQLITE_OPEN_READWRITE,
+    sqlite3.SQLITE_OPEN_CREATE,
+    sqlite3.SQLITE_OPEN_FULLMUTEX)
+local dbh = assert(sqlite3.open_sharable(":memory:", flags))
+dbh:exec [[
+CREATE TABLE t(id INTEGER PRIMARY KEY AUTOINCREMENT, v INTEGER);
+]]
+
+local s = multi.state():load [====[
+local unix = require "dromozoa.unix"
+local sqlite3 = require "dromozoa.sqlite3"
+
+local verbose = os.getenv "VERBOSE" == "1"
+
+local ptr = ...
+local dbh = assert(sqlite3.open_sharable(ptr))
+local sth = assert(dbh:prepare [[
+SELECT COUNT(*) AS n FROM t;
+]])
+
+while true do
+  if verbose then
+    print "SELECT"
+  end
+  assert(sth:step() == sqlite3.SQLITE_ROW)
+  local r = assert(sth:columns())
+  assert(sth:reset())
+  if verbose then
+    print("SELECT done " .. r.n)
+  end
+  if r.n >= 10 then
+    break
+  end
+end
+]====]
+
+local t = multi.thread(s, dbh:share())
+
+local sth = assert(dbh:prepare [[
+INSERT INTO t(v) VALUES(:v);
+]])
+
+for i = 1, 10 do
+  if verbose then
+    print("INSERT " .. i)
+  end
+  assert(sth:bind(":v", i))
+  local result, message = sth:step()
+  if verbose and message then
+    print(message)
+  end
+  assert(sth:reset())
+  if verbose then
+    print "INSERT done"
+  end
+  unix.nanosleep(0.05)
+end
+
+t:join()
